26ac74933835b4f502038349046dc00d13a8f76a
hjpark625 <zero950@gmail.com>
2024-05-25 17:52:29 +0900

    chore: port 환경변수로 변경

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@/app.module'
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule)
   app.enableCors()
-  await app.listen(4000)
+  await app.listen(port)
 }
 
 bootstrap()
