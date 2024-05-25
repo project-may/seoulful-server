2b424c7aafeeb4421e41064e4f9a9df96d4a1b13
hjpark625 <zero950@gmail.com>
2024-05-25 17:53:30 +0900

    refactor: enableCors 옵션 명시

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@/app.module'
 
 async function bootstrap() {
-  const port = process.env.PORT || 4000
+  const PORT = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
-  await app.listen(port)
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  })
+  await app.listen(PORT)
 }
 
 bootstrap()
