490080f215683b17ae0c3d528013220b4949ee59
hjpark625 <zero950@gmail.com>
2024-05-28 11:46:30 +0900

    feat: 로깅 미들웨어 제작

---
+++ src/middleware/loggin.middleware.ts
@@ -0,0 +1,34 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Request, Response, NextFunction } from 'express'
+import { Log } from '@/schema/logs.schema'
+import type { LogModel } from '@/schema/logs.schema'
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  constructor(@InjectModel(Log.name) readonly customLog: LogModel) {}
+
+  private readonly logger = new Logger(LoggingMiddleware.name)
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, ip, originalUrl, hostname } = req
+    const startTime = Date.now()
+
+    res.on('finish', async () => {
+      const { statusCode, statusMessage } = res
+      const responseTime = Date.now() - startTime
+
+      const logMessage = `${method} | ${statusCode} | ${statusMessage} | ${originalUrl} | ${ip} | ${hostname} | ${responseTime}ms`
+
+      if (statusCode >= 500) {
+        this.logger.error(logMessage)
+      } else if (statusCode >= 400) {
+        this.logger.warn(logMessage)
+      } else {
+        this.logger.log(logMessage)
+      }
+    })
+
+    next()
+  }
+}
