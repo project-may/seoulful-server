aa293f65cae8e9df663194e512b37e74ecc556ca
hjpark625 <zero950@gmail.com>
2024-05-28 11:47:37 +0900

    feat: 행사 모듈 및 root 모듈 내 log DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { ScheduleModule } from '@nestjs/schedule'
@@ -6,6 +6,10 @@ import { AppController } from '@/app.controller'
 import { AppService } from '@/app.service'
 import { PushModule } from '@/push/push.module'
 import { EventsModule } from '@/events/events.module'
+import { Log, LogSchema } from '@/schema/logs.schema'
+import { LoggingMiddleware } from '@/middleware/loggin.middleware'
+import { APP_FILTER } from '@nestjs/core'
+import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 @Module({
   imports: [
@@ -15,15 +19,20 @@ import { EventsModule } from '@/events/events.module'
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (env: ConfigService) => ({
-        uri: env.get<string>('MONGO_URI_PRODUCTION')
+        uri: env.get<string>('MONGO_URI_LOCAL')
       }),
       inject: [ConfigService]
     }),
+    MongooseModule.forFeature([{ name: Log.name, schema: LogSchema }]),
     ScheduleModule.forRoot(),
     PushModule,
     EventsModule
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, { provide: APP_FILTER, useClass: HttpExceptionFilter }]
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
