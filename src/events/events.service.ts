4ae3adaca27541e43e98956065a58b5415896eae
hjpark625 <zero950@gmail.com>
2024-05-31 09:58:32 +0900

    feat: 상세 검색 api 서비스 제작 및 리팩토링
    - 일부 endpoint 겹치는 문제로 다른 api endpoint 수정작업 병행

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -14,15 +14,17 @@ import type { EventsModel, IEventData } from '@/events/types/events.type'
 export class EventsService {
   constructor(@InjectModel(Events.name) private eventsModel: EventsModel) {}
 
-  async getEventList(limit: string, offset: string) {
+  async getEventList(limit: string, offset: string, categorySeq?: string) {
     try {
       if (!limit || !offset) throw new BadRequestException('limit 또는 offset 값이 없습니다.')
       if (Number.isNaN(Number(limit)) || Number.isNaN(Number(offset)))
         throw new BadRequestException('유효하지 않은 limit 혹은 offset 값입니다.')
+      if (categorySeq && Number.isNaN(Number(categorySeq)))
+        throw new BadRequestException('유효하지 않은 카테고리입니다.')
 
       const eventListData: IEventData[] = await this.eventsModel
         .find(
-          {},
+          { ...(categorySeq && { category_seq: categorySeq }) },
           {
             _id: 0,
             event_id: 1,
@@ -39,33 +41,12 @@ export class EventsService {
         .skip(Number(offset))
         .exec()
 
-      const totalData = await this.eventsModel.countDocuments().exec()
-      const resultData = eventListData.map((data) => new EventDTO(data))
-      const result = new EventListResponseDTO(resultData, totalData)
-
-      return result
-    } catch (err) {
-      if (err instanceof HttpException) throw err
-      else throw new InternalServerErrorException(err)
-    }
-  }
-
-  async getEventsByCategory(categorySeq: string, limit: string, offset: string) {
-    try {
-      if (categorySeq == null || Number.isNaN(Number(categorySeq)))
-        throw new BadRequestException('유효하지 않은 카테고리 입니다.')
-      if (!limit || !offset) throw new BadRequestException('limit 또는 offset 값이 없습니다.')
-      if (Number.isNaN(Number(limit)) || Number.isNaN(Number(offset)))
-        throw new BadRequestException('유효하지 않은 limit 혹은 offset 값입니다.')
-
-      const eventsListData: IEventData[] = await this.eventsModel
-        .find({ category_seq: categorySeq })
-        .limit(Number(limit))
-        .skip(Number(offset))
+      const totalDataCount = await this.eventsModel
+        .countDocuments({ ...(categorySeq && { category_seq: Number(categorySeq) }) })
         .exec()
-      const totalData = await this.eventsModel.countDocuments({ category_seq: categorySeq }).exec()
-      const resultData = eventsListData.map((data) => new EventDTO(data))
-      const result = new EventListResponseDTO(resultData, totalData)
+
+      const resultData = eventListData.map((data) => new EventDTO(data))
+      const result = new EventListResponseDTO(resultData, totalDataCount)
 
       return result
     } catch (err) {
@@ -83,6 +64,49 @@ export class EventsService {
 
       const detailData = new EventDTO(eventDetailData)
       const result = new EventDetailResponseDTO(detailData)
+
+      return result
+    } catch (err) {
+      if (err instanceof HttpException) throw err
+      else throw new InternalServerErrorException(err)
+    }
+  }
+
+  async getEventListBySearch(
+    limit: string,
+    offset: string,
+    eventName: string,
+    startDate: string,
+    endDate: string,
+    categorySeq?: number,
+    guSeq?: number
+  ) {
+    try {
+      if (!limit || !offset) throw new BadRequestException('limit 또는 offset 값이 없습니다.')
+      if (Number.isNaN(Number(limit)) || Number.isNaN(Number(offset)))
+        throw new BadRequestException('유효하지 않은 limit 혹은 offset 값입니다.')
+      if (!eventName) throw new BadRequestException('검색어가 없습니다.')
+      if (!startDate || !endDate) throw new BadRequestException('시작일 또는 종료일이 없습니다.')
+      if (categorySeq && Number.isNaN(categorySeq)) throw new BadRequestException('유효하지 않은 카테고리입니다.')
+      if (guSeq && Number.isNaN(guSeq)) throw new BadRequestException('유효하지 않은 지역입니다.')
+
+      const query = {
+        event_name: { $regex: eventName, $options: 'i' },
+        start_date: { $gte: startDate },
+        end_date: { $lte: endDate },
+        ...(categorySeq && { category_seq: categorySeq }),
+        ...(guSeq && { gu_seq: guSeq })
+      }
+
+      const eventListData: IEventData[] = await this.eventsModel
+        .find(query)
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .exec()
+      const totalDataCount = await this.eventsModel.countDocuments(query).exec()
+
+      const resultData = eventListData.map((data) => new EventDTO(data))
+      const result = new EventListResponseDTO(resultData, totalDataCount)
 
       return result
     } catch (err) {
