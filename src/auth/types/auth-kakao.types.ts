8ef11d71e05a79c6addf2bb9590addfa523ff8c2
hjpark625 <zero950@gmail.com>
2024-06-01 17:12:28 +0900

    feat: kakao 소셜로그인 응답 인터페이스 정의

---
+++ src/auth/types/auth-kakao.types.ts
@@ -0,0 +1,30 @@
+export interface KakaoTokenResponse {
+  access_token: string
+  token_type: string
+  refresh_token: string
+  id_token: string
+  expires_in: number
+  scope: string
+  refresh_token_expires_in: number
+}
+
+export interface KakaoUserInfoResponse {
+  id: number
+  connected_at: Date
+  kakao_account: {
+    profile_naicname_needs_agreement: boolean
+    profile_image_needs_agreement: boolean
+    profile: {
+      nickname: string
+      thumbnail_image_url: string
+      profile_image_url: string
+      is_defualt_image: boolean
+      is_default_nickname: boolean
+    }
+    has_email: boolean
+    email_needs_agreement: boolean
+    is_email_valid: boolean
+    is_email_verified: boolean
+    email: string
+  }
+}
