6a5c1a1e29e3bdfcbbbe2dccdc729a14b93df4f4
hjpark625 <zero950@gmail.com>
2024-06-01 17:12:58 +0900

    feat: kakao 소셜 로그인 기능 1차 완료

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, HttpCode, HttpStatus, Param, Post } from '@nestjs/common'
+import { AuthService } from '@/auth/auth.service'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login/:provider')
+  @HttpCode(HttpStatus.OK)
+  async getToken(@Param('provider') provider: string, @Body() requestBody: { code: string }) {
+    if (provider === 'kakao') {
+      const accessToken = await this.authService.getKakaoToken(requestBody)
+      const userInfo = await this.authService.getKakaoUserInfo(accessToken)
+      return userInfo
+    }
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import axios, { isAxiosError } from 'axios'
+import type { KakaoTokenResponse, KakaoUserInfoResponse } from './types/auth-kakao.types'
+
+@Injectable()
+export class AuthService {
+  constructor(private configService: ConfigService) {}
+
+  async getKakaoToken(requestBody: { code: string }) {
+    const kakaoTokenUrl = 'https://kauth.kakao.com/oauth/token'
+    const clientId = this.configService.get<string>('KAKAO_API_KEY')
+    const authCode = requestBody.code
+    const payload = {
+      grant_type: 'authorization_code',
+      client_id: clientId,
+      // TODO: redirect_uri논의 필요 및 환경변수 화
+      redirect_uri: 'http://localhost:5173/users/signin/kakao',
+      code: authCode
+    }
+    try {
+      const { data } = await axios.post<KakaoTokenResponse>(kakaoTokenUrl, payload, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+      })
+      console.log('when Success Token', data)
+      return data.access_token
+    } catch (err) {
+      if (isAxiosError(err)) {
+        console.log('when Error Token', err.response.data)
+      }
+    }
+  }
+
+  async getKakaoUserInfo(token: string) {
+    const kakaoUserInfoUrl = 'https://kapi.kakao.com/v2/user/me'
+    const accessToken = `Bearer ${token}`
+    const headers = {
+      Authorization: accessToken,
+      'Contnent-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+    }
+    const propertyKeys = ['kakao_account.email', 'kakao_account.profile', 'kakao_account.name']
+    const queryString = `property_keys=${JSON.stringify(propertyKeys)}`
+    try {
+      const { data } = await axios.get<KakaoUserInfoResponse>(`${kakaoUserInfoUrl}?${queryString}`, { headers })
+      console.log('when Success UserInfo', data)
+
+      return data
+    } catch (err) {
+      if (isAxiosError(err)) {
+        console.log('when Error UserInfo', err.response.data)
+      }
+    }
+  }
+}
