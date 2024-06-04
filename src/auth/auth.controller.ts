96f6e72aecc2e4fb18dd692b5b6b446304e33e05
hjpark625 <zero950@gmail.com>
2024-06-04 19:59:08 +0900

    feat: 소셜로그인 api 완료
    - 추후 로그아웃 및 유저 정보 GET요청 api 작업 필요

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Param, Post } from '@nestjs/common'
+import { BadRequestException, Body, Controller, HttpCode, HttpStatus, Param, Post } from '@nestjs/common'
 import { AuthService } from '@/auth/auth.service'
 
 @Controller('auth')
@@ -7,11 +7,21 @@ export class AuthController {
 
   @Post('/login/:provider')
   @HttpCode(HttpStatus.OK)
-  async getToken(@Param('provider') provider: 'kakao' | 'google' | 'naver', @Body() requestBody: { code: string }) {
+  async getToken(@Param('provider') provider: 'kakao' | 'naver', @Body() requestBody: { code: string; state: string }) {
     if (provider === 'kakao') {
-      const accessToken = await this.authService.getKakaoToken(requestBody)
-      const userInfo = await this.authService.getKakaoUserInfo(accessToken)
-      return userInfo
+      const { access_token, refresh_token } = await this.authService.getKakaoToken(requestBody)
+      const kakaoUserInfo = await this.authService.getKakaoUserInfo(access_token)
+      const result = await this.authService.checkAndSaveUser(kakaoUserInfo, access_token, refresh_token)
+      return result
     }
+    if (provider === 'naver') {
+      const { access_token, refresh_token } = await this.authService.getNaverToken(requestBody)
+      const naverUserInfo = await this.authService.getNaverUserInfo(access_token)
+      const result = await this.authService.checkAndSaveUser(naverUserInfo, access_token, refresh_token)
+      return result
+    }
+    throw new BadRequestException('Invalid provider')
   }
+
+  // TODO: Logout 각 api별로 구현해야함
 }
