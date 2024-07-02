f555b5a69fff5cb69d9152c7bbf19fa9dad4def9
hjpark625 <zero950@gmail.com>
2024-07-02 18:16:30 +0900

    refactor: CORS 대응

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from '@/app.module'
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
+  const appUrl = process.env.APP_URL_LOCAL
+
+  // TODO: CSP 대응 필요
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: appUrl,
+    methods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
     credentials: true
   })
   await app.listen(PORT)
