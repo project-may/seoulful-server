ded3a20fd855946205e66b218015a90a2959f337
hjpark625 <zero950@gmail.com>
2024-07-03 17:46:57 +0900

    refactor: CORS origin 추가

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from '@/app.module'
 async function bootstrap() {
   const PORT = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule, { cors: true })
-  const appUrl = process.env.APP_URL_LOCAL
+  const appUrl = process.env.APP_URL_PRODUCTION
+  const appUrlLocal = process.env.APP_URL_LOCAL
 
   // TODO: CSP 대응 필요
   app.enableCors({
-    origin: appUrl,
+    origin: [appUrl, appUrlLocal],
     methods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
     credentials: true
