54009f149c6199f32fb28b48c03f830225051ea2
hjpark625 <zero950@gmail.com>
2024-07-13 17:21:05 +0900

    feat: 북마크 GET db 컬렉션 연동
    - event 및 user collection

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@nestjs/common'
-import { User, UserModel } from '@/schema/user.schema'
 import { InjectModel } from '@nestjs/mongoose'
+import { User } from '@/schema/user.schema'
+import { Events } from '@/schema/events.schema'
+import type { UserModel } from '@/schema/user.schema'
+import type { EventsModel } from '@/events/types/events.type'
 import type { IUserData } from '@/auth/types/user.types'
+import type { IEventData } from '@/events/types/events.type'
 
 @Injectable()
 export class BookmarkService {
-  constructor(@InjectModel(User.name) private readonly userModel: UserModel) {}
+  constructor(
+    @InjectModel(User.name) private readonly userModel: UserModel,
+    @InjectModel(Events.name) private readonly eventsModel: EventsModel
+  ) {}
 
-  getBookmarkList(userId: string) {
-    return `userId: GET ${userId}`
+  async getBookmarkList(userId: string) {
+    const bookmarkListArr = []
+    const { bookmark_list }: IUserData = await this.userModel.findOne({ user_id: userId })
+
+    if (bookmark_list.length > 0) {
+      for (const eventSeq of bookmark_list) {
+        const event: IEventData = await this.eventsModel.findOne({ event_id: eventSeq })
+        bookmarkListArr.push(event)
+      }
+    }
+
+    return `userId: GET ${userId} bookmark list is ${bookmarkListArr.length === 0 ? 'empty' : bookmarkListArr}`
   }
 
   addBookmark(userId: string, payload: { eventSeq: number }) {
