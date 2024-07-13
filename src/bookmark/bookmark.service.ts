7cd07190449b02b09499ddae865730f30871c82c
hjpark625 <zero950@gmail.com>
2024-07-13 17:00:32 +0900

    feat: bookmark service 제작

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common'
+import { User, UserModel } from '@/schema/user.schema'
+import { InjectModel } from '@nestjs/mongoose'
+import type { IUserData } from '@/auth/types/user.types'
+
+@Injectable()
+export class BookmarkService {
+  constructor(@InjectModel(User.name) private readonly userModel: UserModel) {}
+
+  getBookmarkList(userId: string) {
+    return `userId: GET ${userId}`
+  }
+
+  addBookmark(userId: string) {
+    return `userId: PUT ${userId}`
+  }
+}
