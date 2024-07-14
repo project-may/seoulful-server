8d63d9a8b57329beb6f9cc4f5cedbb1bf7e736f8
hjpark625 <zero950@gmail.com>
2024-07-14 17:40:25 +0900

    refactor: try...catch문 적용 에러 핸들링 추가

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { User } from '@/schema/user.schema'
 import { Events } from '@/schema/events.schema'
@@ -16,32 +16,41 @@ export class BookmarkService {
   ) {}
 
   async getBookmarkList(userId: string) {
-    const { bookmark_list }: IUserData = await this.userModel.findOne({ user_id: userId })
-    const bookmarkCounts = bookmark_list.length
+    try {
+      const user: IUserData = await this.userModel.findOne({ user_id: userId })
+      if (!user) throw new NotFoundException('해당 유저를 찾을 수 없습니다.')
 
-    if (bookmark_list.length === 0) {
-      return new EventListResponseDTO([], bookmarkCounts)
+      const bookmarkCounts = user.bookmark_list.length
+
+      if (user.bookmark_list.length === 0) {
+        return new EventListResponseDTO([], bookmarkCounts)
+      }
+
+      const subQuery = {
+        _id: 0,
+        event_id: 1,
+        category_seq: 1,
+        event_name: 1,
+        period: 1,
+        main_img: 1,
+        start_date: 1,
+        end_date: 1,
+        detail_url: 1
+      }
+
+      const bookmarkEvents = await Promise.all(
+        user.bookmark_list.map(
+          (eventSeq) => this.eventsModel.findOne({ event_id: eventSeq }, subQuery) as Promise<IEventData>
+        )
+      )
+
+      const resultData = bookmarkEvents.map((data) => new EventDTO(data))
+
+      return new EventListResponseDTO(resultData, bookmarkCounts)
+    } catch (err) {
+      if (err instanceof HttpException) throw err
+      else throw new InternalServerErrorException(err)
     }
-
-    const subQuery = {
-      _id: 0,
-      event_id: 1,
-      category_seq: 1,
-      event_name: 1,
-      period: 1,
-      main_img: 1,
-      start_date: 1,
-      end_date: 1,
-      detail_url: 1
-    }
-
-    const bookmarkEvents = await Promise.all(
-      bookmark_list.map((eventSeq) => this.eventsModel.findOne({ event_id: eventSeq }, subQuery) as Promise<IEventData>)
-    )
-
-    const resultData = bookmarkEvents.map((data) => new EventDTO(data))
-
-    return new EventListResponseDTO(resultData, bookmarkCounts)
   }
 
   addBookmark(userId: string, payload: { eventSeq: number }) {
