4c2c949244df08b9340998cf846be7d0891f180c
hjpark625 <zero950@gmail.com>
2024-07-14 17:35:59 +0900

    feat: 북마크 가져오기 완성

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { User } from '@/schema/user.schema'
 import { Events } from '@/schema/events.schema'
+import { EventDTO, EventListResponseDTO } from '@/events/events.dto'
 import type { UserModel } from '@/schema/user.schema'
 import type { EventsModel } from '@/events/types/events.type'
 import type { IUserData } from '@/auth/types/user.types'
@@ -15,17 +16,32 @@ export class BookmarkService {
   ) {}
 
   async getBookmarkList(userId: string) {
-    const bookmarkListArr = []
     const { bookmark_list }: IUserData = await this.userModel.findOne({ user_id: userId })
+    const bookmarkCounts = bookmark_list.length
 
-    if (bookmark_list.length > 0) {
-      for (const eventSeq of bookmark_list) {
-        const event: IEventData = await this.eventsModel.findOne({ event_id: eventSeq })
-        bookmarkListArr.push(event)
-      }
+    if (bookmark_list.length === 0) {
+      return new EventListResponseDTO([], bookmarkCounts)
     }
 
-    return `userId: GET ${userId} bookmark list is ${bookmarkListArr.length === 0 ? 'empty' : bookmarkListArr}`
+    const subQuery = {
+      _id: 0,
+      event_id: 1,
+      category_seq: 1,
+      event_name: 1,
+      period: 1,
+      main_img: 1,
+      start_date: 1,
+      end_date: 1,
+      detail_url: 1
+    }
+
+    const bookmarkEvents = await Promise.all(
+      bookmark_list.map((eventSeq) => this.eventsModel.findOne({ event_id: eventSeq }, subQuery) as Promise<IEventData>)
+    )
+
+    const resultData = bookmarkEvents.map((data) => new EventDTO(data))
+
+    return new EventListResponseDTO(resultData, bookmarkCounts)
   }
 
   addBookmark(userId: string, payload: { eventSeq: number }) {
