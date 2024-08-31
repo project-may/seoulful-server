091332be58b6a85705ebf361ad51b8e1b7b733e1
hjpark625 <zero950@gmail.com>
2024-08-31 20:40:42 +0900

    refactor: 변경된 tsconfig compileOptions에 의한 수정 적용

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from '@/app.module'
 async function bootstrap() {
   const PORT = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule, { cors: true })
-  const appUrl = process.env.APP_URL_PRODUCTION
-  const appUrlLocal = process.env.APP_URL_LOCAL
+  const appUrl = process.env.APP_URL_PRODUCTION || ''
+  const appUrlLocal = process.env.APP_URL_LOCAL || ''
 
   // TODO: CSP 대응 필요
   app.enableCors({
